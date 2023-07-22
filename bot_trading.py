26d756e111141ee8fb652bfef8b885f1a3b83231
Marc Link <linkcla10@gmail.com>
2023-07-23 01:39:02 +0200

    [UPDATE] Changed name and added market trend

--- bot_trading.py
+++ bot_trading.py
@@ -11,14 +11,15 @@ exchange = ccxt.okx({
 
 balance = exchange.fetch_balance()
 
-bars = exchange.fetch_ohlcv('BTC/USDT', timeframe='4h', limit=300)
+bars = exchange.fetch_ohlcv('BTC/USDT', timeframe='5m', limit=300)
 
 df = pd.DataFrame(
     bars, columns=['times', 'open', 'hight', 'low', 'close', 'volume'])
 
+# indicators
 adx = ta.adx(df['hight'], df['low'], df['close'])
 
-ema55 = ta.ema(df['close'], 10)
+ema55 = ta.ema(df['close'], 55)
 
 ema200 = ta.ema(df['close'], 200)
 
@@ -26,7 +27,7 @@ df = pd.concat([df, adx, ema55, ema200], axis=1)
 
 penultRow = df.iloc[-2]
 lastRow = df.iloc[-1]
-#  times     open    hight      low    close    volume    ADX_14     DMP_14     DMN_14        EMA_10       EMA_200
+#  times     open    hight      low    close    volume    ADX_14     DMP_14     DMN_14        EMA_55       EMA_200
 
 
 def get_movement_force():
@@ -45,3 +46,23 @@ def get_movement_force():
         output += 'Movimiento sin fuerza'
 
     return output
+
+
+def get_trend():
+    output = ''
+    if lastRow['EMA_55'] > lastRow['EMA_200']:
+        output += 'Tendencia alcista'
+    elif lastRow['EMA_55'] < lastRow['EMA_200']:
+        output += 'Tendencia bajista'
+    else:
+        # ema55 == ema200
+        if penultRow['EMA_55'] > penultRow['EMA_200']:
+            output += 'Cruce de medias hacia arriba'
+        else:
+            output += 'Cruce de medias hacia abajo'
+
+    return output
+
+
+print(get_movement_force())
+print(get_trend())
