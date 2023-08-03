1fd8b80e50ac1e00db2589a61227afb9092e26e2
Marc Link <linkcla10@gmail.com>
2023-08-03 19:00:20 +0200

    [UPDATE] Doing changes for optimize profits

--- provisional.py
+++ provisional.py
@@ -1,44 +1,7 @@
 # import ccxt
 import pandas_ta as ta
 import pandas as pd
-import matplotlib.pyplot as plt
 import yfinance as yf
-
-
-class Position():
-    def __init__(self, Open_price, take_profit, stop_loss, id):
-        self.Open_price = Open_price
-        self.take_profit = take_profit
-        self.stop_loss = stop_loss
-        self.id = id
-
-    def control_position(self, actual_price):
-        if actual_price >= self.take_profit:
-            PnL.append((self.take_profit/self.Open_price) - 1)
-            win += 1
-            self.delete_position()
-
-        if actual_price <= self.stop_loss:
-            PnL.append((self.stop_loss/self.Open_price) - 1)
-            loss += 1
-            self.delete_position()
-
-    def Close_position(self, actual_price):
-        if actual_price > self.Open_price:
-            profit += (actual_price/self.Open_price) - 1
-            PnL.append((actual_price/self.Open_price) - 1)
-            win += 1
-            self.delete_position()
-
-    def delete_position(self):
-        del positions[self.id]
-
-
-positions: list[Position] = [None] * 100
-PnL = []
-win = 0
-loss = 0
-
 
 # exchange = ccxt.okx({
 #     'apiKey': '29328226-77af-4516-a7b7-d132e61c4f78',
@@ -48,21 +11,22 @@ loss = 0
 
 # bars = exchange.fetch_ohlcv('BTC/USDT', timeframe='4h', limit=300)
 
-btc = yf.Ticker('BTC-USD')
-df = btc.history(period='5y')
-print(df)
+btc = yf.Ticker('META')
+df = btc.history(period='10y', interval='1wk')
 
 
 # df = pd.DataFrame(
 #     bars, columns=['Date', 'Open', 'High', 'Low', 'Close'])
 # df = pd.DataFrame(
 #     bars, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
-df = df[['Date', 'Open', 'High', 'Low', 'Close']]
-print(df)
+# df = df[['Date', 'Open', 'High', 'Low', 'Close']]
+# print(df)
 # ------------------------ INDICATORS -----------------------
 adx = round(ta.adx(df['High'], df['Low'], df['Close']), 2)
 
 rsi = round(ta.rsi(df['Close']), 2)
+
+stochrsi = round(ta.stochrsi(df['Close']), 2)
 
 ema55 = round(ta.ema(df['Close'], 55), 2)
 
@@ -76,8 +40,7 @@ sqz = round(ta.squeeze(df['High'], df['Low'], df['Close']), 2)
 # Concatenate
 
 #  FORMAT:   Date   Open   High   Low   Close    ADX_14   DMP_14   DMN_14   RSI_14   ATRr_14   SQZ_20_2.0_20_1.5   SQZ_ON   SQZ_OFF   SQZ_NO   EMA_55   EMA_200
-df = pd.concat([df, adx, rsi, atr, sqz, ema55, ema200], axis=1)
-
+df = pd.concat([df, adx, atr, sqz, ema55, ema200, stochrsi, rsi], axis=1)
 
 # ------------------------ FUNCTIONS ------------------------
 
@@ -91,7 +54,7 @@ def get_trend(actualCandle):
 
 
 def get_movement_force(oneCandle, twoCandle):
-    MIN_FORCE = 23
+    MIN_FORCE = 0
     bullish_mov_getting_force = False
     barish_mov_lossing_force = False
 
@@ -110,14 +73,34 @@ def get_info_sqz(oneCandle, twoCandle, threeCandle):
     """ This function returns the buy/sell signals of the Squeeze momentum indicator of Lazy Bear"""
 
     if threeCandle['SQZ_20_2.0_20_1.5'] < 0:
-        if (threeCandle['SQZ_20_2.0_20_1.5'] > twoCandle['SQZ_20_2.0_20_1.5']) & (twoCandle['SQZ_20_2.0_20_1.5'] < oneCandle['SQZ_20_2.0_20_1.5']):
+        if (twoCandle['SQZ_20_2.0_20_1.5'] < oneCandle['SQZ_20_2.0_20_1.5']):
             return True, False
 
     elif threeCandle['SQZ_20_2.0_20_1.5'] > 0:
         if (threeCandle['SQZ_20_2.0_20_1.5'] < twoCandle['SQZ_20_2.0_20_1.5']) & (twoCandle['SQZ_20_2.0_20_1.5'] > oneCandle['SQZ_20_2.0_20_1.5']):
             return False, True
 
+    # if threeCandle['SQZ_20_2.0_20_1.5'] < 0:
+    #     if (threeCandle['SQZ_20_2.0_20_1.5'] > twoCandle['SQZ_20_2.0_20_1.5']) & (twoCandle['SQZ_20_2.0_20_1.5'] < oneCandle['SQZ_20_2.0_20_1.5']):
+    #         return True, False
+
+    # elif threeCandle['SQZ_20_2.0_20_1.5'] > 0:
+    #     if (threeCandle['SQZ_20_2.0_20_1.5'] < twoCandle['SQZ_20_2.0_20_1.5']) & (twoCandle['SQZ_20_2.0_20_1.5'] > oneCandle['SQZ_20_2.0_20_1.5']):
+    #         return False, True
+
     return False, False
+
+
+def over_sell_rsi(oneCandle):
+    if oneCandle['RSI_14'] < 30:
+        return True
+    return False
+
+
+def over_sell_stoch(oneCandle):
+    if (oneCandle['STOCHRSIk_14_14_3_3'] < 20) & (oneCandle['STOCHRSIk_14_14_3_3'] < oneCandle['STOCHRSId_14_14_3_3']):
+        return True
+    return False
 
 
 def get_stop_loss(long_or_short, oneCandle):
@@ -133,41 +116,24 @@ def get_take_profit(long_or_short, oneCandle):
     """ This function calculates and returns the price of the take profi """
     # long == true
     if (long_or_short):
-        return round(oneCandle['Close'] + (oneCandle['ATRr_14']*1.5), 2)
+        return round(oneCandle['Close'] + (oneCandle['ATRr_14']*3), 2)
     else:
         return round(oneCandle['Close'] - (oneCandle['ATRr_14']*1.5), 2)
 
-
-def buy(buy_requisits, actualCandle, oneCandle, id):
-
-    buy = True
-    for i in buy_requisits.values():
-        buy &= i
-
-    if buy:
-        positions[id] = Position(actualCandle['Close'], get_take_profit(
-            True, oneCandle), get_stop_loss(True, oneCandle), id)
-        id += 1
-
-    return id
-
-
-def sell(sell_requisits, actualCandle):
-    sell = True
-    for i in sell_requisits.values():
-        sell &= i
-
-    if sell:
-        for position in positions:
-            if type(position) == 'Position':
-                position.Close_position(actualCandle['Close'])
+# -----------------------------------------------------------
 
 
 def main():
-    id = 0
     contador = 0
-    pos = []
-    for i in range(283, len(df)):
+    entry_date = []
+    PnL = []
+    win = 0
+    loss = 0
+    running_position = False
+    entry_price = -1
+    stop_loss = -1
+    take_profit = -1
+    for i in range(204, len(df)):
 
         aux_df = df.head(i + 1)
 
@@ -178,7 +144,9 @@ def main():
         buy_requisits = {
             "val1": False,
             "val2": False,
-            "val3": False
+            "val3": False,
+            "val4": False,
+            "val5": False
         }
         sell_requisits = {
             "val1": False
@@ -193,35 +161,49 @@ def main():
         buy_requisits['val2'] = get_movement_force(oneCandle, twoCandle)
         buy_sell = get_info_sqz(oneCandle, twoCandle, threeCandle)
         buy_requisits['val3'] = buy_sell[0]
-
-        if (buy_requisits['val1'] == True) & (buy_requisits['val2'] == True) & (buy_requisits['val3'] == True):
-            contador += 1
-            pos.append(actualCandle)
+        buy_requisits['val4'] = over_sell_stoch(oneCandle)
+        buy_requisits['val5'] = over_sell_rsi(oneCandle)
 
         sell_requisits['val1'] = buy_sell[1]
 
-        id = buy(buy_requisits, actualCandle, oneCandle, id)
-        sell(sell_requisits, actualCandle)
+        # Open position
+        if buy_requisits['val1'] & buy_requisits['val2'] & buy_requisits['val3'] & (not running_position):
+            running_position = True
+            entry_date.append(actualCandle['Open'])
+            entry_price = actualCandle['Close']
+            stop_loss = get_stop_loss(True, oneCandle)
+            take_profit = get_take_profit(True, oneCandle)
+            contador += 1
 
-        for position in positions:
-            if type(position) == '__main__.Position':
-                position.control_position(actualCandle['Close'])
+        if running_position:
+
+            # Check if we take stop loss
+            if actualCandle['Low'] <= stop_loss:
+                running_position = False
+                loss += 1
+                PnL.append((stop_loss/entry_price) - 1)
+
+            # Check if we have a sell signal of SQZ when we are in a winning position
+            if get_info_sqz(oneCandle, twoCandle, threeCandle)[1]:
+                actual_price = actualCandle['Close']
+                if actual_price > entry_price:
+                    running_position = False
+                    win += 1
+                    PnL.append((actual_price/entry_price) - 1)
+
+            # Check if we take take profit
+            if actualCandle['High'] >= take_profit:
+                running_position = False
+                win += 1
+                PnL.append((take_profit/entry_price) - 1)
 
     print(f'{contador=}')
-    print(f'{pos=}')
-
-    print(f'PnL: {PnL}')
-    print(f'wins: {win}')
-    print(f'losses: {loss}')
-
-    x = pd.to_datetime(df['Date'])
-    y = df['ADX_14']
-    y1 = df['SQZ_20_2.0_20_1.5'] / 100
-    plt.axhline(y=0)
-    plt.axhline(y=23, color='r')
-    plt.plot(x, y, linewidth=2.0, color='r')
-    plt.plot(x, y1, linewidth=2.0)
-    plt.show()
+    # print(f'{entry_date=}')
+    print(f'{PnL=}')
+    print(f'% PnL {sum(PnL) * 100}')
+    print(f'si operaramos con 100$ tendriamos: {100 + sum(PnL)*100}')
+    print(f'{win=}')
+    print(f'{loss=}')
 
 
-# main()
+main()
